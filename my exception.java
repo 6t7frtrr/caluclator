6ac83f55de7e35ebab39a6d9deaae978616cabc5
6t7frtrr <100062256+6t7frtrr@users.noreply.github.com>
2022-03-05 13:45:47 +0530

    Add files via upload

---
+++ my exception.java
@@ -0,0 +1,28 @@
+
+	
+
+
+	@SuppressWarnings("serial")
+	class MyException extends Exception 
+	{ 
+	    public MyException(String s) 
+	    { 
+	        super(s); 
+	    } 
+	} 
+	public class ABCD
+	{ 
+	    public static void main(String args[]) 
+	    { 
+	        try
+	        { 
+	            throw new MyException("end"); 
+	        } 
+	        catch (MyException ex) 
+	        { 
+	            System.out.println("Caught"); 
+	            System.out.println(ex.getMessage()); 
+	        } 
+	    } 
+	}
+
