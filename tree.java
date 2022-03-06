8e0e19e128538b5660c0f1b810a2ebc4f8203eb6
6t7frtrr <100062256+6t7frtrr@users.noreply.github.com>
2022-03-06 13:52:06 +0530

    Add files via upload

---
+++ tree.java
@@ -0,0 +1,19 @@
+package collection.framework;
+import java.util.TreeSet;
+public class treeset {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+TreeSet<String>set=new TreeSet<>();
+set.add("uttt");
+set.add("huhu");
+set.add("bbbh");
+set.add("sjuju");
+set.add("ruff");
+for (String elem:set)
+{
+	System.out.println(elem);
+}
+}
+
+}
